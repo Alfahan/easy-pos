c4eae45c40d8232fe58d6f55770908d07940ea65
Alfahan <ali.farhan160@gmail.com>
2025-01-22 15:23:03 +0700

    feat: eloquent relations

--- app/Models/StockOpnameDetail.php
+++ app/Models/StockOpnameDetail.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class StockOpnameDetail extends Model
 {
     protected $guarded = [];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function stockTotal()
+    {
+        return $this->belongsTo(StockTotal::class, 'product_id', 'product_id');
+    }
 }
