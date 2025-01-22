749086a5edfbcda97e30bd6e5501f331afc96bfa
Alfahan <ali.farhan160@gmail.com>
2025-01-22 12:24:13 +0700

    feat: inertia.js, react.js, laravel

---
+++ resources/js/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
+
+// Mengimpor file CSS Bootstrap 5.3
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Mengimpor file CSS kustom
+import '../css/style.css';
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
+        return pages[`./Pages/${name}.jsx`]
+    },
+
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />)
+    },
+})
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::inertia('/home', 'Home');
+
 Route::get('/', function () {
     return view('welcome');
 });
