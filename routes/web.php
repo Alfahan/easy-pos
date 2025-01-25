18a1e13e5227ef81ff4ef8fca38b9bd0c0b5446a
Alfahan <ali.farhan160@gmail.com>
2025-01-25 14:44:22 +0700

    feat: page authentication

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/home', 'Home');
+Route::redirect('/login', '/');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/', [\App\Http\Controllers\Auth\LoginController::class, 'index'])->name('login');
+    Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'store'])->name('login.store');
 });
+
+Route::post('/logout', [\App\Http\Controllers\Auth\LogoutController::class, '__invoke'])->name('logout')->middleware('auth');
