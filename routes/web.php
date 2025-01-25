3aa84306e6ec1dbe0d72a075acf9e730bf29d16e
Alfahan <ali.farhan160@gmail.com>
2025-01-25 16:39:58 +0700

    feat: page-layout-admin

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,8 @@ Route::middleware('guest')->group(function () {
     Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'store'])->name('login.store');
 });
 
+Route::prefix('admin')->middleware(['auth'])->name('admin.')->group(function () {
+    Route::get('/dashboard', \App\Http\Controllers\Admin\DashboardController::class)->name('dashboard')->middleware('permission:dashboard.index');
+});
+
 Route::post('/logout', [\App\Http\Controllers\Auth\LogoutController::class, '__invoke'])->name('logout')->middleware('auth');
