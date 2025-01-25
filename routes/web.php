50ebea2643551d66fd1ef50f17e9867add06541d
Alfahan <ali.farhan160@gmail.com>
2025-01-25 17:10:04 +0700

    feat: request-controller-roles

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@ Route::middleware('guest')->group(function () {
 
 Route::prefix('admin')->middleware(['auth'])->name('admin.')->group(function () {
     Route::get('/dashboard', \App\Http\Controllers\Admin\DashboardController::class)->name('dashboard')->middleware('permission:dashboard.index');
+
+    $resources = [
+        'roles' => [
+            'controller' => \App\Http\Controllers\Admin\RoleController::class,
+            'permissions' => 'roles.index|roles.create|roles.edit|roles.delete',
+            'names' => 'roles'
+        ],
+    ];
+
+    foreach ($resources as $name => $resource) {
+        $route = Route::resource($name, $resource['controller'])
+            ->middleware("permission:{$resource['permissions']}");
+        if (isset($resource['names'])) {
+            $route->names($resource['names']);
+        }
+    }
 });
 
 Route::post('/logout', [\App\Http\Controllers\Auth\LogoutController::class, '__invoke'])->name('logout')->middleware('auth');
